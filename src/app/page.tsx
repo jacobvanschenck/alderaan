2289033dc989bcff3f7fbe97524fc6aed92c1d99
Jacob Van Schenck <jacob.vanschenck@gmail.com>
2024-09-24 22:18:01 -0500

    fix build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function HomePage() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-			Here's looking at you, kid
+			Here&apos;s looking at you, kid
 		</main>
 	);
 }
